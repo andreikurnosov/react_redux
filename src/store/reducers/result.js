46c1e90386b643d16f3eb1d91b13003c8274e2c5
nixdie <nixdie@yandex.ru>
2019-02-22 10:48:47 +0700

    leaner switch-case statement

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,25 +5,18 @@ const initialState = {
   results: []
 };
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    (result) => result.id !== action.resultElId
+  );
+  return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.STORE_RESULT:
-      return updateObject(state, {
-        results: state.results.concat({
-          id: Math.random(),
-          value: action.result
-        })
-      });
+    case actionTypes.STORE_RESULT: return updateObject(state, {results: state.results.concat({id: Math.random(), value: action.result})});
+    case actionTypes.DELETE_RESULT: return deleteResult(state, action);
 
-    case actionTypes.DELETE_RESULT:
-      // const id = 2;
-      // const newArray = [...state.results];
-      // state.results.splice(id, 1);
-      // results: newArray
-      const updatedArray = state.results.filter(
-        (result) => result.id !== action.resultElId
-      );
-      return updateObject(state, { results: updatedArray });
     default:
       return state;
   }
